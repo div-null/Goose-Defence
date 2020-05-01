339f0690f5fac22de475eae0beb714e1772cce1a
KillFrost01 <maidannik.nikita@yandex.ru>
2020-05-01 17:36:16 +0700

    Добавлены классы Goose, GooseFabric

---
+++ Assets/Scripts/GooseFabric.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooseFabric : MonoBehaviour
+{
+    public bool fabricActivity;                     //активность фабрики
+    public List<GameObject> goose_prefabs;          //префабы гусей
+    public List<Goose> geese;                       //ГУУУУСИИИИ
+
+    public IEnumerator SpawnGeese(int level, List<GameObject> prefabs)
+    {
+        int count = 5 + level * 2;                  //формуля для вычисления кол-ва гусей
+        for(int i = 0; i < count; i++)
+        {
+            var goose = GameObject.Instantiate(prefabs[Random.Range(0, 2)]);    //случайный префаб
+
+            //добавление гуся
+            geese.Add(new Goose(
+                level                                      //левел
+            ));
+            goose.AddComponent<Goose>();
+
+            yield return null;
+        }
+        
+    }
+
+    public GooseFabric()
+    {
+        fabricActivity = true;
+        goose_prefabs = new List<GameObject>();
+        geese = new List<Goose>();
+    }
+}
---
+++ Assets/Scripts/StartF.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartF : MonoBehaviour
+{
+    public List<GameObject> goose_prefabs;          //префабы гусей
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GooseFabric fabric = new GooseFabric();
+        StartCoroutine(fabric.SpawnGeese(1,goose_prefabs));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
