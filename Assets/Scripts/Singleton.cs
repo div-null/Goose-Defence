016505c8bf1c8ec0c7a5f2cf6dc892b2178ee079
Optifun <frdima19@gmail.com>
2020-05-01 16:36:20 +0700

    Singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> where T: MonoBehaviour
+{
+    T instance;
+    public T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<T>();
+            }
+            return instance;
+        }
+    }
+
+    bool isExists { get { return instance != null; } }
+
+}
