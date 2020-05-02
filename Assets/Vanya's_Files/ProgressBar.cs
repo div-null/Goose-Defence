f12a7887f14df7125f1997b4f27ec6b284088526
dirtybobby <vanya.frolov2001@gmail.com>
2020-05-02 22:34:44 +0700

    UI с моделями
    Изменены интерфейсы в главном меню и в игре:
    1. Изменена информация об обхекте
    2. Изменена покупка объекта
    3. Добавлен выход из игры
    4. Добавлен  переход (некорректно работает)

---
+++ Assets/Vanya's_Files/ProgressBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Image progress;
+    public float procents;
+    // Start is called before the first frame update
+    void Start()
+    {
+         progress = progress.GetComponentInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        progress.fillAmount = procents;
+    }
+}
--- Assets/Vanya's_Files/UI_manager.cs
+++ Assets/Vanya's_Files/UI_manager.cs
@@ -8,43 +8,44 @@ public class UI_manager : MonoBehaviour
     //"Папки с текстом либо с картинкой" main menu
     public GameObject background, history, pressKeyToStart;
     //"Папки с текстом либо с картинкой" game
-    public GameObject goldStat, gooseStat, difficultStat;
+    public GameObject moneyStat, dangerStat;
     public GameObject infoPanel;
+    public GameObject buyPanel;
+    public GameObject UIInMenu, UIInGame;
     //Если игра не запущена, значит мы в меню, если нет, то UI надо изменить
     bool isGameStarted = false, canSkip = false;
     string historyStr = "Весь мир был поражён вирусом, из-за которого гуси стали неимоверно большыми, голодными, и практически поработили человечество с помощью звона колокольчиков. Единственное, что осталось у людей - это большой колокол и фермы, на которых они выращивают корм чтобы кормить гусей. Это последняя надежда человечества. С минуты на минуту гуси начнут своё последнее наступление, защитите колокол!";
+    [Header("InfoAboutTower")]
+    public Button Accept;
+    public Text infoAboutTower, damage, radius, speed, reload, cost;
+    public Text upgradeDamage, upgradeRadius, upgradeSpeed, upgradeReload;
+    public Animator transitor;
 
-    void UI_TurnOnMenu()
+    public void UI_TurnOnMenu()
     {
-        infoPanel.SetActive(false);
-        background.SetActive(true);
-        history.SetActive(true);
-        pressKeyToStart.SetActive(true);
-        goldStat.SetActive(false);
-        gooseStat.SetActive(false);
-        difficultStat.SetActive(false);
+        transitor.SetBool("Status", false);
+        UIInMenu.SetActive(true);
         StartCoroutine("ReadHistory");
-
+        UIInGame.SetActive(false);
+        history.GetComponentInChildren<Text>().text = "";
+        canSkip = false;
+        isGameStarted = false;
+        transitor.SetBool("Status", true);
     }
 
     void UI_TurnOnGame()
     {
-        background.SetActive(false);
-        history.SetActive(false);
-        pressKeyToStart.SetActive(false);
-        goldStat.SetActive(true);
-        gooseStat.SetActive(true);
-        difficultStat.SetActive(true);
+        transitor.SetBool("Status", false);
+        UIInMenu.SetActive(false);
+        infoPanel.SetActive(false);
+        buyPanel.SetActive(false);
+        UIInGame.SetActive(true);
+        transitor.SetBool("Status", true);
     }
 
     void UI_SetAmountOfGold(int amount)
     {
-        goldStat.GetComponent<Text>().text = amount.ToString();
-    }
-
-    void UI_SetAmountOfGoose(int dead, int total)
-    {
-        gooseStat.GetComponent<Text>().text = dead.ToString() + "/" + total.ToString();
+        moneyStat.GetComponent<Text>().text = amount.ToString();
     }
 
     // Start is called before the first frame update
@@ -52,6 +53,11 @@ public class UI_manager : MonoBehaviour
     {
         UI_TurnOnMenu();
         pressKeyToStart.GetComponent<Text>().text = "Нажмите на любую клавишу, чтобы пропустить историю";
+    }
+
+    public void CloseWindow()
+    {
+        Application.Quit();
     }
 
     void ShowHistory()
@@ -64,7 +70,43 @@ public class UI_manager : MonoBehaviour
         infoPanel.SetActive(false);
     }
 
+    public void CloseBuyPanel()
+    {
+        buyPanel.SetActive(false);
+    }
 
+    public void SelectFirstTypeOfTower()
+    {
+        BuyTower(1);
+    }
+
+    public void SelectSecondTypeOfTower()
+    {
+        BuyTower(2);
+    }
+
+    public void SelectThirdTypeOfTower()
+    {
+        BuyTower(3);
+    }
+
+    public void BuyTower(int id)
+    {
+        infoPanel.SetActive(true);
+        upgradeDamage.gameObject.SetActive(false);
+        upgradeRadius.gameObject.SetActive(false);
+        upgradeSpeed.gameObject.SetActive(false);
+        upgradeReload.gameObject.SetActive(false);
+        Accept.GetComponentInChildren<Text>().name = "Купить";
+        //if (moneyStat.GetComponentInChildren<Text>().text >= cost) Accept.GetComponent<Text>().enabled = true;
+        //else Accept.GetComponent<Text>().enabled = false;
+        buyPanel.SetActive(false);
+    }
+
+    void UpgradeTower(int id, int level)
+    {
+        Accept.GetComponent<Text>().name = "Улучшить";
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -73,6 +115,18 @@ public class UI_manager : MonoBehaviour
         {
             isGameStarted = true;
             UI_TurnOnGame();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+            if (hit)
+            {
+                if (hit.transform.tag == "Player")
+                    infoPanel.SetActive(true);
+                else if (hit.transform.tag == "Respawn")
+                    buyPanel.SetActive(true);
+            }
         }
     }
 
