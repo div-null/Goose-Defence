4378c2177e8ca5d4ef77054ecf9a6eb19b34593f
Optifun <frdima19@gmail.com>
2020-05-03 19:21:00 +0700

    Fix brunch

--- Assets/Scripts/StartF.cs
+++ Assets/Scripts/StartF.cs
@@ -12,13 +12,13 @@ public class StartF : MonoBehaviour
     void Start()
     {
         //TowerFabric.Instance.placeTower(0, new TowerStatsList.TowerTomatoT1());
-        TowerFabric.Instance.placeTower(1, new TowerStatsList.TowerCabbageT2());
-        TowerFabric.Instance.placeTower(2, new TowerStatsList.TowerPeasT2());
-        TowerFabric.Instance.placeTower(3, new TowerStatsList.TowerTomatoT2());
+        //TowerFabric.Instance.placeTower(1, new TowerStatsList.TowerCabbageT2());
+        //TowerFabric.Instance.placeTower(2, new TowerStatsList.TowerPeasT2());
+        //TowerFabric.Instance.placeTower(3, new TowerStatsList.TowerTomatoT2());
       //  TowerFabric.Instance.placeTower(4, new TowerStatsList.TowerCabbageT3());
 
 
-        Game.Instance.startGame();
+        //Game.Instance.startGame();
     }
 
     // Update is called once per frame
