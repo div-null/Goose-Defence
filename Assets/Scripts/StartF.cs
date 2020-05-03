54abd7a4b8f1af9ebeade6bd37937d775f5729db
KillFrost01 <maidannik.nikita@yandex.ru>
2020-05-03 18:19:57 +0700

    Правка анимаций

--- Assets/Scripts/StartF.cs
+++ Assets/Scripts/StartF.cs
@@ -11,11 +11,11 @@ public class StartF : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TowerFabric.Instance.placeTower(0, new TowerStatsList.TowerTomatoT1());
-        TowerFabric.Instance.placeTower(2, new TowerStatsList.TowerCabbageT1());
-        TowerFabric.Instance.placeTower(1, new TowerStatsList.TowerTomatoT2());
-        TowerFabric.Instance.placeTower(3, new TowerStatsList.TowerTomatoT3());
-        TowerFabric.Instance.placeTower(4, new TowerStatsList.TowerCabbageT3());
+        //TowerFabric.Instance.placeTower(0, new TowerStatsList.TowerTomatoT1());
+        TowerFabric.Instance.placeTower(1, new TowerStatsList.TowerCabbageT2());
+        TowerFabric.Instance.placeTower(2, new TowerStatsList.TowerPeasT2());
+        TowerFabric.Instance.placeTower(3, new TowerStatsList.TowerTomatoT2());
+      //  TowerFabric.Instance.placeTower(4, new TowerStatsList.TowerCabbageT3());
 
 
         Game.Instance.startGame();
