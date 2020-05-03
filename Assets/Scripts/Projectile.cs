bd8a3d6884f62751f5c1f2167eec35f516847381
Optifun <frdima19@gmail.com>
2020-05-03 11:43:24 +0700

    Projectile move z

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -67,8 +67,10 @@ public class Projectile : MonoBehaviour
     {
         if (RemainTime > 0)
         {
-            transform.Rotate(0, 0, 10f * Time.deltaTime);                       //вращение снаряда        
-            transform.position += Direction * Velocity * Time.deltaTime;
+            transform.Rotate(0, 0, 10f * Time.deltaTime);                       //вращение снаряда     
+            Vector3 newpos = transform.position + Direction * Velocity * Time.deltaTime;
+            newpos.z= -3 + Mathf.Abs(newpos.y / 10);
+            transform.position = newpos;
             RemainTime -= Time.deltaTime;
         }
         else
