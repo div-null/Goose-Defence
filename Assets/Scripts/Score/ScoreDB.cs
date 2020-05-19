46730efa6d32f3c9e558ada37aee702652b6eab9
Optifun <frdima19@gmail.com>
2020-05-19 18:06:56 +0700

    Score DataBase and adaptive camera
    Добавлен:
    - Подсчет и вывод статистики
      + Шаблон для БД
      + Интерфейс для вывода
      + Префабы составляющие этот интерфейс
    - Немного изменил ландшафт
    - Добавил зумирование для камеры, изменил цвет задника

---
+++ Assets/Scripts/Score/ScoreDB.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+[CreateAssetMenu(fileName ="Score Database Template", menuName = "Score Database")]
+public class ScoreDB : ScriptableObject
+{
+    [Description("Количество свободных мест в топе")]
+    public int TopNumber = 5;
+    List<int> scores;
+    
+    private void OnDestroy ()
+        {
+        UpLoadScore();
+        }
+
+    public void Load ()
+        {
+        scores = new List<int>(LoadScore());
+        scores.Sort();
+        scores.Reverse();
+        }
+
+    /// <summary>
+    /// Размещает очки в списке
+    /// </summary>
+    /// <param name="score"></param>
+    public void AddScore(int score)
+        {
+        List<int> result = new List<int>(scores);
+        result.Add(score);
+        result.Sort();
+        result.Reverse();
+        scores = result.GetRange(0, scores.Count);
+        UpLoadScore();
+        }
+
+    /// <summary>
+    /// Получает список очков
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<int> GetScore ()
+        {
+        return scores;
+        }
+
+    /// <summary>
+    /// Загружает данные из хранилища
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerable<int> LoadScore ()
+        {
+        List<int> loaded = new List<int>();
+        string key;
+        for ( int i = 0; i < TopNumber; i++ )
+            {
+            key = $"score{i}";
+            if ( PlayerPrefs.HasKey(key) )
+                loaded.Add(PlayerPrefs.GetInt(key));
+            else
+                loaded.Add(-1);
+            }
+        return loaded; 
+        }
+
+    /// <summary>
+    /// Выгружает данные в хранилище
+    /// </summary>
+    private void UpLoadScore ()
+        {
+        string key;
+        for ( int i = 0; i < TopNumber; i++ )
+            {
+            key = $"score{i}";
+            PlayerPrefs.SetInt(key, scores[i]);
+            }
+        }
+}
---
+++ Assets/Scripts/Score/ScoreTable.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTable : Singleton<ScoreTable>
+{
+    /// <summary>
+    /// Настройки бд
+    /// </summary>
+    [SerializeField]
+    ScoreDB DB;
+
+    /// <summary>
+    /// Полотно для дочерних элементов
+    /// </summary>
+    [SerializeField]
+    Canvas canv;
+
+    /// <summary>
+    /// Лейбл "ТОП 5"
+    /// </summary>
+    [SerializeField]
+    Text TopScore;
+
+    /// <summary>
+    /// Префаб панели счёта
+    /// </summary>
+    [SerializeField]
+    GameObject textPanelPref;
+
+    /// <summary>
+    /// Контейнер для счёта
+    /// </summary>
+    [SerializeField]
+    Transform Content;
+    private void Start ()
+        {
+        canv.enabled = false;
+        DB.Load();
+        }
+
+    private void Reset ()
+        {
+        DB = new ScoreDB();
+        canv = GetComponent<Canvas>();
+        TopScore = transform.Find("TopScore").GetComponent<Text>();
+        Content = transform.Find("Table").Find("Viewport").Find("Content");
+        }
+
+    public void Show (int current)
+        {
+        canv.enabled = true;
+
+        DB.AddScore(current);
+        TopScore.text = $"Top {DB.TopNumber} scores:";
+        List<int> scores = new List<int>(DB.GetScore());
+
+        // Spawn new panels
+        for ( int i = 0; i < scores.Count; i++ )
+            {
+            var label = Instantiate(textPanelPref, Content, false).GetComponent<Text>();
+            if ( scores[i] == -1 )
+                label.text = $"{i + 1}. ----------";
+            else
+                label.text = $"{i + 1}. {scores[i]}";
+            }
+        }
+
+    public void Hide ()
+        {
+        canv.enabled = false;
+        // Destroy child objects
+        foreach ( var field in Content.GetComponentsInChildren<Text>() )
+            Destroy(field);
+        }
+}
