a7819aac10d989caf2eb79302651df63b1ad0251
Dmitry Frolov <frdima19@gmail.com>
2021-01-02 16:21:14 +0700

    prettify Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Projectile : MonoBehaviour
 {
 	public Animator animator;
@@ -36,18 +37,29 @@ public class Projectile : MonoBehaviour
 	/// </summary>
 	public float timeSlow;
 
-
+	/// <summary>
+	/// Направление полёта снаряда
+	/// </summary>
 	Vector3 Direction;
 
+	/// <summary>
+	/// Снаряд закончил падение
+	/// </summary>
+	bool isLanded;
+
+	/// <summary>
+	/// Оставшееся время до падения
+	/// </summary>
 	float RemainTime;
-	public void Loauch(Vector3 tower, Vector3 point, ProjectileStats stats)
+
+	public void Loauch (Vector3 tower, Vector3 point, ProjectileStats stats)
 	{
-		isStop = false;
+		isLanded = false;
 		Damage = stats.Damage;
 		this.Velocity = stats.Velocity;
 		transform.position = tower;
-		Direction = (point - tower).normalized;
-		RemainTime = (Vector3.Distance(tower, point) / stats.Velocity);
+		Direction = ( point - tower ).normalized;
+		RemainTime = Vector3.Distance(tower, point) / stats.Velocity;
 		Radius = stats.ExplosionRange;
 
 		this.coefSlow = stats.coefSlow;
@@ -56,39 +68,40 @@ public class Projectile : MonoBehaviour
 		transform.rotation = Quaternion.LookRotation(Vector3.back);
 	}
 
-	void MakeDamage()
+	void MakeDamage ()
 	{
 		Vector2 pos = transform.position;
 		// TODO: Вызов метода дамага гусей
 		GooseFabric.Instance.OnAttack(Radius, pos, Damage, coefSlow, timeSlow);
-		StartCoroutine("Destroy");
-
+		StartCoroutine(Destroy());
 	}
-	bool isStop;
-	private void Start()
+
+
+	private void Start ()
 	{
 		animator = GetComponent<Animator>();
 	}
-	void FixedUpdate()
+
+	void FixedUpdate ()
 	{
-		if (RemainTime > 0)
+		if ( RemainTime <= 0 && !isLanded )
+		{
+			isLanded = true;
+			MakeDamage();
+		}
+
+		if ( RemainTime > 0 )
 		{
 			// transform.Rotate(0, 0, 10f * Time.deltaTime);                       //вращение снаряда     
 			Vector3 newpos = transform.position + Direction * Velocity * Time.deltaTime;
+			RemainTime -= Time.deltaTime;
 			newpos.z = -3 + Mathf.Abs(newpos.y / 10);
 			transform.position = newpos;
-			RemainTime -= Time.deltaTime;
 		}
-		else
-		{
-			if (!isStop)
-			{
-				isStop = true;
-				MakeDamage();
-			}
-		}
+
 	}
-	IEnumerator Destroy()
+
+	IEnumerator Destroy ()
 	{
 		animator.SetTrigger("Destroy");
 		yield return new WaitForSeconds(1f);
